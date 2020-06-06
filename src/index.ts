d3a0c7e2d42fcd9ba418fd5a21e41239f8db4172
fabio <fabio.forcolin@gmail.com>
2020-06-06 11:06:17 +0200

    Send notification to device via htpp callback from CLoud Tasks

--- src/index.ts
+++ src/index.ts
@@ -3,28 +3,40 @@ import * as admin from 'firebase-admin'
 
 const {CloudTasksClient} = require('@google-cloud/tasks');
 
-interface ExpiringDocumentData extends admin.firestore.DocumentData {
-    expiresIn?: number
-    expiresAt?: admin.firestore.Timestamp
+interface DocumentData extends admin.firestore.DocumentData {
+    sendIn?: number
+    registrationToken?: string
 }
 
 interface ExpirationTaskPayload {
     docPath: string
 }
 
+interface RegistrationTokenPayload {
+    registrationToken: string
+}
+
 admin.initializeApp();
 
-export const onCreatePost =
-    functions.region("europe-west1").firestore.document('/posts/{id}').onCreate(async snapshot => {
-        const data = snapshot.data()! as ExpiringDocumentData;
-        const {expiresIn, expiresAt} = data;
-        let expirationAtSeconds: number | undefined;
-        if (expiresIn && expiresIn > 0) {
-            expirationAtSeconds = Date.now() / 1000 + expiresIn
-        } else if (expiresAt) {
-            expirationAtSeconds = expiresAt.seconds
+export const onUpdateUserData =
+    functions.region("europe-west1").firestore.document('/users/{id}').onUpdate(async snapshot => {
+        const data = snapshot.after.data()! as DocumentData;
+        const {sendIn, registrationToken} = data;
+        if (sendIn == undefined) {
+            console.log("sendIn is undefined: " + sendIn);
+            return;// throw error
         }
-        if (!expirationAtSeconds) {
+
+        if (registrationToken == undefined) {
+            console.log("Registration token is undefined: " + registrationToken);
+            return;// throw error
+        }
+
+        let sendAtSecond: number | undefined;
+        if (sendIn && sendIn > 0) {
+            sendAtSecond = Date.now() / 1000 + sendIn
+        }
+        if (!sendAtSecond) {
             // No expiration set on this document
             return
         }
@@ -36,8 +48,8 @@ export const onCreatePost =
         const queuePath: string =
             tasksClient.queuePath(project, location, queue);
         const url = `https://${location}-${project}.cloudfunctions.net/sendNotification`;
-        const docPath = snapshot.ref.path;
-        const payload: ExpirationTaskPayload = {docPath};
+        const payload: RegistrationTokenPayload = {registrationToken};
+
         const task = {
             httpRequest: {
                 httpMethod: 'POST',
@@ -48,41 +60,31 @@ export const onCreatePost =
                 },
             },
             scheduleTime: {
-                seconds: expirationAtSeconds
+                seconds: sendAtSecond
             }
         };
         await tasksClient.createTask({parent: queuePath, task});
     });
 
 
-export const firestoreTtlCallback =
-    functions.region("europe-west1").https.onRequest(async (req, res) => {
-        const payload = req.body as ExpirationTaskPayload;
-        try {
-            console.log("Deleting document...");
-            await admin.firestore().doc(payload.docPath).delete();
-            res.sendStatus(200)
-        } catch (error) {
-            console.error(error);
-            res.status(500).send(error)
-        }
-    });
-
 export const sendNotification =
     functions.region("europe-west1").https.onRequest(async (req, res) => {
+        const httpPayload = req.body as RegistrationTokenPayload;
         // This registration token comes from the client FCM SDKs.
-        console.log("I am sending a fake notification.");
-        var registrationToken = 'e6WCepJgaE8:APA91bHZ8yg5hU0rjAfo8Gy-qV6Zir1ivrcc6GBYccBFBdRXW8rN5uc3OmYBBJu6WxKHL4J_fTYrS6VrmwrKWHw1jfoAusOA5uxKRdYezsfhNXoNXLRaF7NIihwIFXhexRNlCxtj5_c5';
+        console.log("I am sending a notification.");
+        let registrationToken = httpPayload.registrationToken;
+        //var registrationToken = 'e6WCepJgaE8:APA91bHZ8yg5hU0rjAfo8Gy-qV6Zir1ivrcc6GBYccBFBdRXW8rN5uc3OmYBBJu6WxKHL4J_fTYrS6VrmwrKWHw1jfoAusOA5uxKRdYezsfhNXoNXLRaF7NIihwIFXhexRNlCxtj5_c5';
         // See the "Defining the message payload" section above for details
         // on how to define a message payload.
-        var payload = {
+
+        let payload = {
             notification: {
                 title: 'Yooooo!',
                 body: 'Urgent action is needed to prevent your account from being disabled!'
             }
         };
 //       // Set the message as high priority and have it expire after 24 hours.
-        var options = {
+        let options = {
             priority: 'high',
             timeToLive: 60 * 60 * 24
         };
@@ -101,4 +103,17 @@ export const sendNotification =
         // writing to the Firebase Realtime Database.
         // Setting an "uppercase" sibling in the Realtime Database returns a Promise.
         // return snapshot.({'uppercase': uppercase});
+    });
+
+export const firestoreTtlCallback =
+    functions.region("europe-west1").https.onRequest(async (req, res) => {
+        const payload = req.body as ExpirationTaskPayload;
+        try {
+            console.log("Deleting document...");
+            await admin.firestore().doc(payload.docPath).delete();
+            res.sendStatus(200)
+        } catch (error) {
+            console.error(error);
+            res.status(500).send(error)
+        }
     });
