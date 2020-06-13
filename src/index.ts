22a6afca49fc7fe6780b9980e2707323a47e39b3
fabio <fabio.forcolin@gmail.com>
2020-06-13 15:47:23 +0200

    ok

--- src/index.ts
+++ src/index.ts
@@ -4,53 +4,53 @@ import * as field from "./firestore-field-filter";
 
 const {CloudTasksClient} = require('@google-cloud/tasks');
 
-interface DocumentData extends admin.firestore.DocumentData {
-    sendIn?: number
-    registrationToken?: string
+interface UserData extends admin.firestore.DocumentData {
+    longestStreak?: number
+    nextClickAt?: string
+    streak?: number
 }
 
-interface ExpirationTaskPayload {
-    docPath: string
-}
-
+//interface ExpirationTaskPayload {
+//    docPath: string
+//}
+//
 interface RegistrationTokenPayload {
     registrationToken: string
 }
 
 admin.initializeApp();
 
+// Look for changes in "streak" field of any user. If there is a change, schedule a notification in google tasks.
+// The notification is sent to the gameData/registrationToken after gameData/notificationInterval seconds.
 export const onUpdateUserData =
-    functions.region("europe-west1").firestore.document('/users/{id}')
-        .onUpdate(field.default('numberOfHit', 'CHANGED', async (change: any, context: any) => {
-            const data = change.after.data()! as DocumentData;
-            const {sendIn, registrationToken} = data;
-            if (sendIn == undefined) {
-                console.log("sendIn is undefined: " + sendIn);
-                return;// throw error
-            }
+    functions.region("europe-west1").firestore.document('/{user}/userData')
+        .onUpdate(field.default('streak', 'CHANGED', async (change: any, context: any) => {
+            const data = change.after.data()! as UserData;
+            console.log("Data changed for user: " + context.params.user);
 
-            if (registrationToken == undefined) {
-                console.log("Registration token is undefined: " + registrationToken);
-                return;// throw error
-            }
+            const documentSnapshot = await admin.firestore().collection(context.params.user).doc("gameData").get();
+            const registrationToken = documentSnapshot.get("registrationToken");
+            const notificationInterval = documentSnapshot.get("notificationInterval");
 
             let sendAtSecond: number | undefined;
-            if (sendIn && sendIn > 0) {
-                sendAtSecond = Date.now() / 1000 + sendIn
+            if (notificationInterval && notificationInterval > 0) {
+                sendAtSecond = Date.now() / 1000 + notificationInterval;
             }
             if (!sendAtSecond) {
-                // No expiration set on this document
-                return
+                console.log("SendAtSecond undefined: " + sendAtSecond);
+                return;
             }
+            console.log("Streak is: " + data?.streak);
+            console.log("Notification interval is: " + notificationInterval);
             // Get the project ID from the FIREBASE_CONFIG env var
             const project = JSON.parse(process.env.FIREBASE_CONFIG!).projectId;
             const location = 'europe-west1';
-            const queue = 'firestore-ttl';
+            const queue = 'notificationsQueue';
             const tasksClient = new CloudTasksClient();
             const queuePath: string =
-                tasksClient.queuePath(project, location, queue);
+                tasksClient.queuePath(project, "europe-west3", queue);
             const url = `https://${location}-${project}.cloudfunctions.net/sendNotification`;
-            const payload: RegistrationTokenPayload = {registrationToken};
+            const payload: RegistrationTokenPayload = {registrationToken}
 
             const task = {
                 httpRequest: {
@@ -75,14 +75,13 @@ export const sendNotification =
         // This registration token comes from the client FCM SDKs.
         console.log("I am sending a notification.");
         let registrationToken = httpPayload.registrationToken;
-        //var registrationToken = 'e6WCepJgaE8:APA91bHZ8yg5hU0rjAfo8Gy-qV6Zir1ivrcc6GBYccBFBdRXW8rN5uc3OmYBBJu6WxKHL4J_fTYrS6VrmwrKWHw1jfoAusOA5uxKRdYezsfhNXoNXLRaF7NIihwIFXhexRNlCxtj5_c5';
         // See the "Defining the message payload" section above for details
         // on how to define a message payload.
 
         let payload = {
             notification: {
-                title: 'Yooooo!',
-                body: 'Urgent action is needed to prevent your account from being disabled!'
+                title: 'Hi!',
+                body: 'It is time to press the button, don\'t miss it!'
             }
         };
 //       // Set the message as high priority and have it expire after 24 hours.
@@ -105,17 +104,4 @@ export const sendNotification =
         // writing to the Firebase Realtime Database.
         // Setting an "uppercase" sibling in the Realtime Database returns a Promise.
         // return snapshot.({'uppercase': uppercase});
-    });
-
-export const firestoreTtlCallback =
-    functions.region("europe-west1").https.onRequest(async (req, res) => {
-        const payload = req.body as ExpirationTaskPayload;
-        try {
-            console.log("Deleting document...");
-            await admin.firestore().doc(payload.docPath).delete();
-            res.sendStatus(200)
-        } catch (error) {
-            console.error(error);
-            res.status(500).send(error)
-        }
     });
